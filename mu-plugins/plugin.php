65bafb6c7b31e28175df743dd72d04c1e2395cef
david <david.chandra@10up.com>
2019-04-22 09:32:46 +0700

    Add mu-plugins

---
+++ mu-plugins/plugin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: TenUpScaffold
+ * Plugin URI:
+ * Description:
+ * Version:     0.1.0
+ * Author:      10up
+ * Author URI:  https://10up.com
+ * Text Domain: tenup-scaffold
+ * Domain Path: /languages
+ *
+ * @package TenUpScaffold
+ */
+// Useful global constants.
+define( 'TENUP_SCAFFOLD_VERSION', '0.1.0' );
+define( 'TENUP_SCAFFOLD_URL', plugin_dir_url( __FILE__ ) );
+define( 'TENUP_SCAFFOLD_PATH', plugin_dir_path( __FILE__ ) );
+define( 'TENUP_SCAFFOLD_INC', TENUP_SCAFFOLD_PATH . 'includes/' );
+
